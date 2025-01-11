6fe411a515159159537cd91465d9f9cbcb772ec6
Jihun Park <jihunpark.tech@gmail.com>
2025-01-11 21:37:03 +0900

    feat: init project

---
+++ src/main/kotlin/com/jesus/voice/VoiceApplication.kt
@@ -0,0 +1,11 @@
+package com.jesus.voice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VoiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<VoiceApplication>(*args)
+}
