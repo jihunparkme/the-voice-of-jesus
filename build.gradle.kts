6313c76f51927f18eec26896871130ed25100bac
Jihun Park <jihunpark.tech@gmail.com>
2025-01-11 21:38:59 +0900

    feat: init project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
+	val kotlinVersion = "1.9.25"
+	kotlin("jvm") version kotlinVersion
+	kotlin("plugin.spring") version kotlinVersion
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
 }
