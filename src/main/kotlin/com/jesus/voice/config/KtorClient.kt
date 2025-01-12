07bc0a09f8b3f827442e6aa32487a6aaa1031e80
Jihun Park <jihunpark.tech@gmail.com>
2025-01-12 15:03:32 +0900

    test: KtorClient

---
+++ src/main/kotlin/com/jesus/voice/config/KtorClient.kt
@@ -0,0 +1,42 @@
+package com.jesus.voice.config
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.serialization.kotlinx.xml.xml
+import org.springframework.stereotype.Component
+
+@Component
+class KtorClient {
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation) { // JSON, XML 데이터 직렬화 및 역직렬화 플러그인
+            json()
+            xml()
+        }
+
+        install(Logging) { // 요청 및 응답 로그 플러그인
+            level = LogLevel.INFO
+        }
+
+        install(DefaultRequest) { // 모든 요청에 기본적으로 포함될 헤더
+            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+        }
+
+        install(HttpTimeout) { // 요청 타임아웃
+            requestTimeoutMillis = 10_000
+        }
+
+        install(HttpRequestRetry) { // 요청 실패 시 재시도
+            retryOnException(maxRetries = 3)
+        }
+    }
+}
---
+++ src/test/kotlin/com/jesus/voice/config/KtorClientTest.kt
@@ -0,0 +1,32 @@
+package com.jesus.voice.config
+
+import com.jesus.voice.common.IntegrationTest
+import io.kotest.common.runBlocking
+import io.kotest.matchers.shouldBe
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import org.junit.jupiter.api.Test
+
+@IntegrationTest
+class KtorClientTest(
+    private val ktorClient: KtorClient,
+) {
+    @Test
+    fun extractYoutubeTranscript() {
+        runBlocking {
+            val response: HttpResponse = ktorClient.client.get("https://jsonplaceholder.typicode.com/posts/1")
+
+            response.status.value shouldBe 200
+            response.status.description shouldBe "OK"
+            response.body<String>() shouldBe """
+                {
+                  "userId": 1,
+                  "id": 1,
+                  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+                }
+            """.trimIndent()
+        }
+    }
+}
