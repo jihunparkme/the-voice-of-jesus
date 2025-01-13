9db050a656cca761eef977846e0ae84074aa2b93
Jihun Park <jihunpark.tech@gmail.com>
2025-01-13 21:40:58 +0900

    feat: add uil and const

---
+++ src/main/kotlin/com/jesus/voice/common/util/Utils.kt
@@ -0,0 +1,5 @@
+package com.jesus.voice.common.util
+
+import mu.KotlinLogging
+
+val logger = KotlinLogging.logger {}
---
+++ src/main/kotlin/com/jesus/voice/youtube/dto/Const.kt
@@ -0,0 +1,5 @@
+package com.jesus.voice.youtube.dto
+
+object Const {
+    const val YOUTUBE_WATCH_URL: String = "https://www.youtube.com/watch?v="
+}
