42d9cb2d3e6994be0ad04435b7647a7add837da2
Jihun Park <jihunpark.tech@gmail.com>
2025-01-14 21:25:24 +0900

    feat: add YoutubeClient

--- src/main/kotlin/com/jesus/voice/youtube/client/YoutubeClient.kt
+++ src/main/kotlin/com/jesus/voice/youtube/client/YoutubeClient.kt
@@ -6,27 +6,28 @@ import com.jesus.voice.youtube.dto.Const.YOUTUBE_WATCH_URL
 import io.ktor.client.call.body
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.isSuccess
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Component
 
 @Component
 class YoutubeClient(
     private val ktorClient: KtorClient,
 ) {
-    suspend fun getVideoPage(videoId: String): String {
-        return runCatching {
-            val response = ktorClient.get(YOUTUBE_WATCH_URL + videoId)
-            handleResponse(response, videoId)
+    fun getVideoPage(videoId: String): String =
+        runCatching {
+            runBlocking {
+                val response = ktorClient.get(YOUTUBE_WATCH_URL + videoId)
+                handleResponse(response, videoId)
+            }
         }.onFailure {
             logger.error("video page request exception. videoId: $videoId, error: ${it.message}", it)
         }.getOrElse { "" }
-    }
 
-    private suspend fun handleResponse(response: HttpResponse, videoId: String): String {
-        return if (response.status.isSuccess()) {
-            response.body() // body는 suspend 함수이므로 호출 가능
+    private suspend fun handleResponse(response: HttpResponse, videoId: String): String =
+        if (response.status.isSuccess()) {
+            response.body()
         } else {
             logger.error("Video page request failed. videoId: $videoId, status: ${response.status.value}")
             ""
         }
-    }
 }
