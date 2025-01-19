e362903bd3ea18ad0797e8975b81653083f0d0c9
Jihun Park <jihunpark.tech@gmail.com>
2025-01-19 21:32:54 +0900

    test: MorphemeAnalyzerTest

---
+++ src/main/kotlin/com/jesus/voice/komoran/MorphemeAnalyzer.kt
@@ -0,0 +1,57 @@
+package com.jesus.voice.komoran
+
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL
+import kr.co.shineware.nlp.komoran.core.Komoran
+import kr.co.shineware.nlp.komoran.model.Token
+
+object MorphemeAnalyzer {
+    private val komoran = Komoran(DEFAULT_MODEL.LIGHT)
+    private val NOUNS = setOf("NNG", "NNP")
+    private val ENDINGS = setOf("다EC", "다EF")
+
+    fun analyze(contents: String): Pair<String, WordCount> {
+        val replacedContents = contents.replace("\n", " ")
+
+        val analysisResults = komoran.analyze(replacedContents)
+        val tokenList = analysisResults.tokenList
+
+        val refinedContents = applyNewLine(tokenList, replacedContents)
+        val wordCount = getWordCount(tokenList)
+
+        return Pair(refinedContents, wordCount)
+    }
+
+    private fun getWordCount(tokenList: List<Token>): Map<String, Int> =
+        tokenList
+            .filter { NOUNS.contains(it.getPos()) }
+            .groupingBy { it.getMorph() }
+            .eachCount().entries
+            .sortedByDescending { it.value }
+            .associate { it.key to it.value }
+
+    private fun applyNewLine(
+        tokenList: List<Token>,
+        replacedContents: String
+    ): String = replacedContents.insertNewlines(
+        tokenList
+            .filter { "${it.getMorph()}${it.getPos()}}".containsAny() }
+            .map { it.getEndIndex() }
+    )
+
+    private fun String.insertNewlines(positions: List<Int>): String =
+        buildString {
+            var offset = 0
+            val originalBuilder = StringBuilder(this@insertNewlines)
+            positions.forEach { pos ->
+                require(pos + offset in 0..originalBuilder.length) { "Position $pos is out of range for the string." }
+                originalBuilder.insert(pos + offset, "\n")
+                offset++
+            }
+            append(originalBuilder)
+        }
+
+    private fun String.containsAny(): Boolean =
+        ENDINGS.any { this.contains(it) }
+}
+
+typealias WordCount = Map<String, Int>
---
+++ src/test/kotlin/com/jesus/voice/komoran/MorphemeAnalyzerTest.kt
@@ -0,0 +1,21 @@
+package com.jesus.voice.komoran
+
+import io.kotest.matchers.shouldBe
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.test.Test
+
+class MorphemeAnalyzerTest {
+    @Test
+    fun success_analyze() {
+        val resource = javaClass.classLoader.getResource("transcript/transcript.txt")
+        val contents = Files.readString(Paths.get(resource.toURI()))
+
+        val (refinedContents, wordCount) = MorphemeAnalyzer.analyze(contents)
+
+        println(refinedContents)
+        wordCount.toList().get(0) shouldBe ("기도" to 146)
+        wordCount.toList().get(1) shouldBe ("하나님" to 53)
+        wordCount.toList().get(2) shouldBe ("말씀" to 28)
+    }
+}
