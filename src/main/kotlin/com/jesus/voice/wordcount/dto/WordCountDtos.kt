a11cdab7be71c0cd9450b380369d709d481071a5
Jihun Park <jihunpark.tech@gmail.com>
2025-01-24 15:54:14 +0900

    feat: WordCountService

--- src/main/kotlin/com/jesus/voice/wordcount/dto/WordCountDtos.kt
+++ src/main/kotlin/com/jesus/voice/wordcount/dto/WordCountDtos.kt
@@ -1,10 +1,14 @@
 package com.jesus.voice.wordcount.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class SummarizeRequest(
     val text: String,
     val locale: String = "ko",
 )
 
+@Serializable
 data class SummarizeResponse(
     val mac: String,
     val content: String,
