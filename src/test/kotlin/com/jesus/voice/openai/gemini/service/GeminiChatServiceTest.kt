eb75dd4d1d2c05fe131715d3282f817fd7823e7c
Jihun Park <jihunpark.tech@gmail.com>
2025-01-25 22:33:01 +0900

    feat: GeminiChatServiceTest

--- src/test/kotlin/com/jesus/voice/openai/gemini/service/GeminiChatServiceTest.kt
+++ src/test/kotlin/com/jesus/voice/openai/gemini/service/GeminiChatServiceTest.kt
@@ -1,13 +1,11 @@
 package com.jesus.voice.openai.gemini.service
 
 import com.jesus.voice.common.IntegrationTest
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.test.context.ActiveProfiles
 import java.nio.file.Files
 import java.nio.file.Paths
 
-@Disabled
 @ActiveProfiles("local")
 @IntegrationTest
 class GeminiChatServiceTest(
@@ -18,12 +16,9 @@ class GeminiChatServiceTest(
         val resource = javaClass.classLoader.getResource("transcript/transcript.txt")
         val content = Files.readString(Paths.get(resource.toURI()))
 
-        val result = geminiChatService.chat("아래 글은 교회의 설교 내용입니다. 짧게 요약해 주세요.\n\n$content").refine()
+        val result = geminiChatService.chat("아래 글은 교회의 설교 내용입니다. 내용을 요약해 주세요.\n\n$content")
 
         println(result)
     }
-
-    fun String.refine() = this.replace("  ", "\n")
-            .replace("\n\n", "\n")
 }
 
