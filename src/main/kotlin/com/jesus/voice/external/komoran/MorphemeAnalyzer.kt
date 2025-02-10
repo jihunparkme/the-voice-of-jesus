a8e124a04013d16d9b037fcce61c72769f442dc7
Jihun Park <jihunpark.tech@gmail.com>
2025-02-10 21:13:32 +0900

    refactor: edit filterValues

--- src/main/kotlin/com/jesus/voice/external/komoran/MorphemeAnalyzer.kt
+++ src/main/kotlin/com/jesus/voice/external/komoran/MorphemeAnalyzer.kt
@@ -30,7 +30,7 @@ object MorphemeAnalyzer {
             .eachCount().entries
             .sortedByDescending { it.value }
             .associate { it.key to it.value }
-            .filterValues { it > 10 }
+            .filterValues { it > 9 }
 
     private fun applyNewLine(
         tokenList: List<Token>,
