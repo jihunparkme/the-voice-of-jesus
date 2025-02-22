3c912176bd4cdc1cff8bbc81011896878e8c2afb
Jihun Park <jihunpark.tech@gmail.com>
2025-02-22 20:18:32 +0900

    feat: BasicResponse

---
+++ src/main/kotlin/com/jesus/voice/common/dtos/BasicResponse.kt
@@ -0,0 +1,101 @@
+package com.jesus.voice.common.dtos
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
+
+class BasicResponse<T>(
+    private val status: Int = 0,
+    private val httpStatusCode: HttpStatusCode,
+    private val success: Boolean = false,
+    private val message: String = "",
+    private val count: Int = 0,
+    private val data: T? = null,
+) {
+
+    companion object {
+        fun internalServerError(message: String): ResponseEntity<BasicResponse<Any>> {
+            val basicResponse = BasicResponse<Any>(
+                status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR,
+                success = false,
+                message = message,
+            )
+            return ResponseEntity(basicResponse, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+
+        fun clientError(message: String): ResponseEntity<Any> {
+            val basicResponse = BasicResponse<Any>(
+                status = HttpStatus.BAD_REQUEST.value(),
+                httpStatusCode = HttpStatus.BAD_REQUEST,
+                success = false,
+                message = message,
+            )
+            return ResponseEntity(basicResponse, HttpStatus.BAD_REQUEST)
+        }
+
+        fun notFound(message: String): ResponseEntity<Any> {
+            val basicResponse = BasicResponse<Any>(
+                status = HttpStatus.NOT_FOUND.value(),
+                httpStatusCode = HttpStatus.NOT_FOUND,
+                success = false,
+                message = message,
+            )
+            return ResponseEntity(basicResponse, HttpStatus.NOT_FOUND)
+        }
+
+        fun ok(data: Boolean): ResponseEntity<BasicResponse<Boolean>> {
+            val basicResponse = BasicResponse(
+                status = HttpStatus.OK.value(),
+                httpStatusCode = HttpStatus.OK,
+                success = true,
+                data = data,
+            )
+            return ResponseEntity.ok(basicResponse)
+        }
+
+        fun <T> ok(data: T): ResponseEntity<BasicResponse<T>> {
+            val basicResponse = BasicResponse(
+                status = HttpStatus.OK.value(),
+                httpStatusCode = HttpStatus.OK,
+                success = true,
+                count = 1,
+                data = data,
+            )
+            return ResponseEntity.ok(basicResponse)
+        }
+
+        fun <T> ok(data: List<T>): ResponseEntity<BasicResponse<List<T>>> {
+            val basicResponse = BasicResponse(
+                status = HttpStatus.OK.value(),
+                httpStatusCode = HttpStatus.OK,
+                success = true,
+                count = data.size,
+                data = data,
+            )
+            return ResponseEntity.ok(basicResponse)
+        }
+
+        fun <T> created(data: T): ResponseEntity<BasicResponse<T>> {
+            val basicResponse = BasicResponse(
+                status = HttpStatus.CREATED.value(),
+                httpStatusCode = HttpStatus.CREATED,
+                success = true,
+                count = 1,
+                data = data,
+            )
+            return ResponseEntity(basicResponse, HttpStatus.CREATED)
+        }
+
+        fun <T> created(data: List<T>): ResponseEntity<BasicResponse<List<T>>> {
+            val basicResponse = BasicResponse(
+                status = HttpStatus.CREATED.value(),
+                httpStatusCode = HttpStatus.CREATED,
+                success = true,
+                count = data.size,
+                data = data,
+            )
+            return ResponseEntity(basicResponse, HttpStatus.CREATED)
+        }
+    }
+}
