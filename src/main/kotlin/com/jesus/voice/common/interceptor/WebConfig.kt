60f4f3a0c1e07b9f10864bab34c5387789f2a7d4
Jihun Park <jihunpark.tech@gmail.com>
2025-03-08 21:16:15 +0900

    feat: edit favicon to head

--- src/main/kotlin/com/jesus/voice/common/interceptor/WebConfig.kt
+++ src/main/kotlin/com/jesus/voice/common/interceptor/WebConfig.kt
@@ -2,7 +2,9 @@ package com.jesus.voice.common.interceptor
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.resource.PathResourceResolver
 
 @Configuration
 class WebConfig(
@@ -11,5 +13,12 @@ class WebConfig(
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(validateAdminInterceptor)
+    }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/favicon.ico")
+            .addResourceLocations("classpath:/static/favicon.ico/")
+            .resourceChain(false)
+            .addResolver(PathResourceResolver())
     }
 }
