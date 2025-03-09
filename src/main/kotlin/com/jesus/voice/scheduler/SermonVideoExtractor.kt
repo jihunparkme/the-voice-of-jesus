3ef8bab132487fe7b44f71661f7ac82e9bf46b3e
Jihun Park <jihunpark.tech@gmail.com>
2025-03-09 21:09:52 +0900

    refactor: extract service

--- src/main/kotlin/com/jesus/voice/scheduler/SermonVideoExtractor.kt
+++ src/main/kotlin/com/jesus/voice/scheduler/SermonVideoExtractor.kt
@@ -1,15 +1,8 @@
 package com.jesus.voice.scheduler
 
-import com.jesus.voice.aggregate.sermon.domain.Sermon
-import com.jesus.voice.aggregate.sermon.domain.SermonRepository
+import com.jesus.voice.aggregate.sermon.service.SermonExtractService
 import com.jesus.voice.common.dtos.AYMCPlayList
-import com.jesus.voice.common.dtos.PlayListChannel
 import com.jesus.voice.common.util.logger
-import com.jesus.voice.external.komoran.MorphemeAnalyzer
-import com.jesus.voice.external.openai.gemini.service.GeminiChatService
-import com.jesus.voice.external.youtube.dto.PlayListVideo
-import com.jesus.voice.external.youtube.dto.VideoId
-import com.jesus.voice.external.youtube.service.YoutubeService
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -19,9 +12,7 @@ import org.springframework.stereotype.Component
 
 @Component
 class SermonVideoExtractor(
-    private val youtubeService: YoutubeService,
-    private val sermonRepository: SermonRepository,
-    private val geminiChatService: GeminiChatService,
+    private val sermonExtractService: SermonExtractService,
     @Value("\${app.random.start}") val randomStart: Long,
     @Value("\${app.random.end}") val randomEnd: Long,
 ) {
@@ -36,39 +27,10 @@ class SermonVideoExtractor(
     ).forEach { channel ->
         runBlocking {
             launch {
-                saveSermonVideo(channel)
+                sermonExtractService.extractSermon(channel, channel.id, 1)
                 sleepRandomDuration()
             }
         }
-    }
-
-    private fun saveSermonVideo(channel: PlayListChannel) =
-        youtubeService.getVideoIdFromPlayList(channel.id)
-            .take(1)
-            .filterNot { sermonRepository.existsByVideoId(it.videoId) }
-            .map { generateSermon(it, channel) }
-            .forEach { sermon ->
-                sermonRepository.save(sermon)
-                log.info("✅✅✅ Sermon saved: $sermon")
-            }
-
-    private fun generateSermon(playListVideo: PlayListVideo, channel: PlayListChannel): Sermon {
-        val transcript = runCatching {
-            youtubeService.getTranscript(VideoId(playListVideo.videoId))
-        }.getOrDefault("")
-        if (transcript.isBlank()) {
-            return playListVideo.toSermon(
-                playList = channel.toDocument()
-            )
-        }
-        val (refinedContent, wordCount) = MorphemeAnalyzer.analyze(transcript)
-        val summarizedContent = geminiChatService.chat(CHAT_PREFIX + refinedContent)
-        return playListVideo.toSermon(
-            playList = channel.toDocument(),
-            transcript = refinedContent,
-            summary = summarizedContent,
-            wordCount = wordCount,
-        )
     }
 
     private suspend fun sleepRandomDuration() {
@@ -79,7 +41,6 @@ class SermonVideoExtractor(
 
     companion object {
         private val log by logger()
-        private val CHAT_PREFIX = "아래 글은 교회의 설교 내용입니다. 내용을 요약해 주세요.\n\n"
     }
 }
 
