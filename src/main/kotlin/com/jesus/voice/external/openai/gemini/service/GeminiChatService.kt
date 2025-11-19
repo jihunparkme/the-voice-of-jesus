101f4a38f4d34d7ee3c0e547ae6b6b0885c47cfc
Jihun Park <jihunpark.tech@gmail.com>
2025-11-19 20:32:50 +0900

    feat: apply genaiClient

--- src/main/kotlin/com/jesus/voice/external/openai/gemini/service/GeminiChatService.kt
+++ src/main/kotlin/com/jesus/voice/external/openai/gemini/service/GeminiChatService.kt
@@ -1,27 +1,40 @@
 package com.jesus.voice.external.openai.gemini.service
 
+import com.google.genai.Client
 import com.jesus.voice.common.exception.GeminiChatException
 import com.jesus.voice.common.exception.WordCountException
 import com.jesus.voice.common.util.logger
-import com.jesus.voice.external.openai.gemini.client.GeminiChatClient
 import org.springframework.stereotype.Service
 
 @Service
 class GeminiChatService(
-    private val geminiChatClient: GeminiChatClient
+    private val genaiClient: Client,
 ) {
     private val log by logger()
 
     @Throws(GeminiChatException::class)
     fun chat(text: String): String =
         runCatching {
-            geminiChatClient.chat(text).getOrThrow()
-                .candidates.first().content.parts.first().text
-                .convertMarkdown()
+            val response = genaiClient.models.generateContent(
+                MODEL_NAME,
+                text,
+                null
+            )
+
+            val answer = response.text()
+            if (answer == null) {
+                throw WordCountException("문장 요약에 실패하였습니다.")
+            }
+            return answer
         }.onFailure {
             log.error(it.message, it)
             throw WordCountException("문장 요약에 실패하였습니다.")
         }.getOrDefault(text)
+
+    companion object {
+        // https://ai.google.dev/gemini-api/docs/models?hl=ko#model-versions
+        private const val MODEL_NAME = "gemini-flash-latest"
+    }
 }
 
 fun String.convertMarkdown(): String {
